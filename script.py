06e562c58af0721f1ada1589515b44d22da1f98f
mohamedsaad308 <mohamedsaad308@gmail.com>
2023-05-18 13:21:08 +0300

    add sending messages to sqs

--- script.py
+++ script.py
@@ -10,11 +10,18 @@ def get_ssm_parameter(parameter_name):
     return response["Parameter"]["Value"]
 
 
+def send_sqs_message(queue_name, message):
+    sqs = boto3.resource("sqs")
+    queue = sqs.get_queue_by_name(QueueName=queue_name)
+    response = queue.send_message(MessageBody=json.dumps(message))
+    return response
+
+
 def disable_email(email):
-    blazmeter_account_id = get_ssm_parameter("blazmeterAccountId")
+    blazemeter_account_id = get_ssm_parameter("blazmeterAccountId")
     blazemeter_key = get_ssm_parameter("blazemeterKey")
 
-    users_api = f"https://a.blazemeter.com/api/v4/accounts/{blazmeter_account_id}/users"
+    users_api = f"https://a.blazemeter.com/api/v4/accounts/{blazemeter_account_id}/users"
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json",
@@ -22,24 +29,33 @@ def disable_email(email):
     }
 
     response = requests.get(users_api, headers=headers)
-    result = response.json()["result"]
+    result = response.json().get("result", [])
 
-    email_id = next(
-        (record["id"] for record in result if record["email"] == email and record["enabled"]),
-        None,
-    )
+    email_records = [record for record in result if record.get("email") == email and record.get("enabled")]
+    if not email_records:
+        message = {
+            "email": email,
+            "deleted": False,
+            "message": f"The email '{email}' does not exist or is already disabled!",
+        }
+        send_sqs_message("deletedFail", message)
+        print(message["message"])
+        return
 
-    if not email_id:
-        print(f"The email '{email}' does not exist or is already disabled!")
-        sys.exit()
-
+    email_id = email_records[0].get("id")
     disable_url = f"{users_api}/{email_id}"
     payload = json.dumps({"enabled": False})
 
     response = requests.put(disable_url, headers=headers, data=payload)
 
     if response.ok:
-        print(f"The email '{email}' was deleted successfully!")
+        message = {
+            "email": email,
+            "deleted": True,
+            "message": f"The email '{email}' was deleted successfully!",
+        }
+        send_sqs_message("deletedSuccess", message)
+        print(message["message"])
 
 
 if __name__ == "__main__":
